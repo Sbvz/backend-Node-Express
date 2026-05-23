c09ba68de686eec4ad21dd38abdb121f83f2a7d1
Sebastian <sebaswcrack2007@gmail.com>
2026-05-23 13:18:53 -0500

    fix: remover .env y corregir configuracion

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use('/api/registros', require('./routes/registros'));
 // ======================================================
 // Puerto
 // ======================================================
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
